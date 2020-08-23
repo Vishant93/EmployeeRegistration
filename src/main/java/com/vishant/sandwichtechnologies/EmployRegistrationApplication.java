90d74d7c0302a0ed7c7dc4a4b58c83721d9df8eb
Vishant93 <vishantk@gmail.com>
2020-08-22 21:31:59 -0700

    Base app

---
+++ src/main/java/com/vishant/sandwichtechnologies/EmployRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.vishant.sandwichtechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishant/sandwichtechnologies/EmployRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishant.sandwichtechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
