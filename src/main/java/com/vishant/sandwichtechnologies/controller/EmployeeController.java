a06b4d4e5ccb68950a5b4cefec98813f397cc906
Vishant93 <vishantk@gmail.com>
2020-08-24 23:03:46 -0700

    front end stuff

--- src/main/java/com/vishant/sandwichtechnologies/controller/EmployeeController.java
+++ src/main/java/com/vishant/sandwichtechnologies/controller/EmployeeController.java
@@ -3,13 +3,17 @@ package com.vishant.sandwichtechnologies.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vishant.sandwichtechnologies.repository.EmployeeRepository;
 import com.vishant.sandwichtechnologies.model.Employee;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
@@ -20,5 +24,11 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
+	}
+	
+	// create employee
+	@PostMapping("employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
